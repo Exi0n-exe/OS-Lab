61c206b97c332bc7dd78f4568c254ccdebbe3183
Varun A L <79893116+Exi0n-exe@users.noreply.github.com>
2023-07-18 09:44:09 +0000

    18-07

--- 18-07-2023/bankersalgo.c
+++ 18-07-2023/bankersalgo.c
@@ -1,21 +1,31 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 void main()
 {
-    int k=0,output[10],d=0,t=0,ins[5],i,avail[5],allocated[10][5],need[10][5],max[10][5],pno,j,rz,count=0;
+    int k = 0, output[10], d = 0, t = 0, ins[5], i, avail[5], allocated[10][5], need[10][5], max[10][5], pno, j, rz, count = 0;
     printf("\n Enter the number of resources : ");
     scanf("%d", &rz);
     printf("\n Enter the max instances of each resources\n");
-    for (i=0;i<rz;i++)
+    for (i = 0; i < rz; i++)
     {
-        avail[i]=0;
-        printf("%c= ",(i+97));
-        scanf("%d",&ins[i]);
+        avail[i] = 0;
+        printf("%c= ", (i + 97));
+        scanf("%d", &ins[i]);
     }
     printf("\n Enter the number of processes : ");
     scanf("%d", &pno);
-    printf("\n Enter the allocation matrix \n     ");
-    for (i=0;i<rz;i++)
-        printf(" %c",(i+97));
-
+    printf("\n Enter the allocation matrix \n ");
+    for (i = 0; i < rz; i++)
+        printf(" %c", (i + 97));
+    printf("\n");
+    for (i = 0; i < pno; i++)
+    {
+        p[i] = i
+            printf("P[%d]  ", p[i]);
+        for (j = 0; j < rz; j++)
+        {
+            scanf("%d", &allocated[i][j]);
+            avail[j] += allocated[i][j];
+        }
+    }
 }
